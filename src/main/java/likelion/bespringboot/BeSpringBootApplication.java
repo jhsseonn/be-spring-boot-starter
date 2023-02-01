b16e76f801eb6c5de233e01bf57fb928f1ec019b
jhsseonn <hs125bt@gmail.com>
2023-02-01 17:24:51 +0900

    Initial commit

---
+++ src/main/java/likelion/bespringboot/BeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package likelion.bespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion/bespringboot/BeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.bespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
